8ea79f0675c037032807d0afb8f75be201bf8135
Said Maadan <saidmaadan@users.noreply.github.com>
2016-01-25 15:32:41 -0600

    Integrated ckeditor for jobs templates

--- app/models/company.rb
+++ app/models/company.rb
@@ -13,11 +13,11 @@ class Company < ActiveRecord::Base
   friendly_id :slug_companies, use: :slugged
   searchkick
   
-  INDUSTRY = ['Accounting', 'Airlines/Aviation', "Alternative Disputs Resolution", "Alternative Medicine", "Animation", "Apparel & Fashion", 
+  INDUSTRY = ['Accounting', 'Airlines/Aviation', 'Alcoholic beverage', "Alternative Disputs Resolution", "Alternative Medicine", "Animation", "Apparel & Fashion", 
     "Architecture & Planning", "Arts & Crafts", "Automotive", "Banking", "Biotechnology", "Broadcast Media", "Building Materials", "Capital Markets", "Commercial Real Estate",
    "Computer Software", "Computer hardware & Network Security", "Construction", 'Consumer Electronics', 'Consumer Services', "Dairy", "Education Management", "E-Learning", "Entertainment", "Financial Services", "Information Technology and Services",
    "Hospital & Health Care", "Import and Export", "Internet", "Government", "Legal", "Management Consulting", "Marketing and Advertising", "Military", "Nonprofit Organization Management",
-   "Oil & Energy","Professional Training & Coaching", "Restaurants", "Research", "Security and Investigations", "Sports", "Staffing and Recruiting", "Transportation", "Venture Capital & Private Equity", "Other"]
+   "Oil & Energy","Professional Training & Coaching", "Restaurants", "Research", "Security and Investigations", "Sports", "Staffing and Recruiting", "Transportation", "Venture Capital & Private Equity", 'Wine and Spirits', "Other"]
   SIZE = ["1 - 10 Employees", "11 - 50 Employees", "51 - 100 Employees", "101 - 200 Employees", "201 - 500 Employees", "501 - 1000 Employees", "1001 - 5000 Employees", "5001 - 10000 Employees", "10001 - 20000 Employees", "20001 - 50000 Employees", "50001 - 100000 Employees", "100001 - 200000 Employees", "200000+ Employees"]
   
   def slug_companies
@@ -31,5 +31,9 @@ class Company < ActiveRecord::Base
   end
   def company_job
     (company.jobs).size
+  end
+
+  def company_jobs
+    jobs.select { |job| job.company_name == company.name }
   end
 end
