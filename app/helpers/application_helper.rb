a8df0775162e711acee3d3e88aec96f624ae2488
Said Maadan <saidmaadan@users.noreply.github.com>
2016-02-15 15:48:39 -0600

    Setup dynamic title and description meta tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def title(title)
+    content_for(:title) { title }
+  end
+
+  def meta_description(description)
+    content_for(:meta_description) { description }
+  end
 end
